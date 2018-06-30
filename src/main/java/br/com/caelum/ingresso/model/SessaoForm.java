3a4fa2a7be5591962fe928efeb450fb7615fee9a
nancy <nancy@nancy>
2018-06-30 15:23:41 -0300

    sessaoForm

--- src/main/java/br/com/caelum/ingresso/model/SessaoForm.java
+++ src/main/java/br/com/caelum/ingresso/model/SessaoForm.java
@@ -2,27 +2,22 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 
 public class SessaoForm	{
+	
 	private	Integer	id;
 	
 	@NotNull
 	private	Integer	salaId;
 	
 	@DateTimeFormat(pattern="HH:mm")
-	@NotNull
-	
+	@NotNull	
 	private	LocalTime horario;
 	
 	@NotNull
